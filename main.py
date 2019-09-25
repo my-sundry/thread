78e017b3e2cc5ab766c0d6d13876f15bd88d7a12
zyftank <12702313@qq.com>
2019-09-25 13:33:49 +0800

    modify main.py file

--- main.py
+++ main.py
@@ -1,65 +1,73 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+'''
+    Author： 
+    Date:    2019-09-25
+'''
 
 import threading
 import datetime
 import sys
-import inspect
-import ctypes
+import pandas as pd
+import tushare as ts
 
-class A:
-    count = 0
-    shutdown_signal = True
-    def fun2(self):
-        global count
+today = int(datetime.date.today().strftime('%Y%m%d'))
+df = pd.read_csv('./trade_cal.csv')
+df = df[df['cal_date'] == today]
+is_open = df['is_open'].tolist()[0]
+
+class Thread:
+    shutdown_signal = True   #退出信号
+    def __init__(self,code,high,low):
+        self.stock_code = code
+        self.high_price_limit = high
+        self.low_price_limit = low
+
+    def price_monitoring(self):
         global shutdown_signal
+        realtime_price = self.get_price()
         if self.shutdown_signal:
-            if self.count < 5:
-                print('线程2执行!')
-                global timer1
-                timer1 = threading.Timer(5,self.fun2)
-                timer1.start()
-                self.count += 1
-            else:
+            if realtime_price > self.high_price_limit or realtime_price <self.low_price_limit:
+                self.send_mail()
                 self.shutdown_signal = False
                 self.timer1.cancel()
+            else:
+                #print(realtime_price)
+                global timer1
+                timer1 = threading.Timer(5,self.price_monitoring)
+                timer1.start()
         else:
             self.timer1.cancel()
 
-    def fun_timer(self):
+    def time_control(self):
         now = datetime.datetime.now().strftime('%H:%M')
         global shutdown_signal
         if self.shutdown_signal:
-            if now < '17:01':
+            if now < '15:31':
                 print('线程1执行!')
                 global timer
-                timer = threading.Timer(10,self.fun_timer)
+                timer = threading.Timer(10,self.time_control)
                 timer.start()
-                #print(timer.isAlive())
             else:
-                #self.terminator(self.timer1)
                 self.shutdown_signal = False
                 self.timer.cancel()
         else:
             self.timer.cancel()
     
-    def __async_raise(self,thread_Id,exctype):
-        thread_Id = ctypes.c_long(thread_Id)
-        if not inspect.isclass(exctype):
-            exctype = type(exctype)
-        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_Id,ctypes.py_object(exctype))
-        if res == 0:
-            raise ValueError("invalid thread id")
-        elif res != 1:
-            ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_Id,None)
-            raise SystemError("PyThreadState_SEtAsyncExc failed")
+    def get_price(self):
+        df = ts.get_realtime_quotes(self.stock_code)
+        price = float(df['b1_p'].tolist()[0])
+        return price
 
-    def terminator(self,thread):
-        self.__async_raise(thread.ident, SystemExit)
+    def send_mail(self):
+        print('sendmail')
 
-if __name__ == '__main__'
-    a = A()
-    a.timer = threading.Timer(1,a.fun_timer)
-    a.timer.start()
-    a.timer1 = threading.Timer(2,a.fun2)
-    a.timer1.start()
+if __name__ == '__main__':
+    if is_open:
+        thread = Thread('511900',100.03,99.97)
+        thread.timer = threading.Timer(1,thread.time_control)
+        thread.timer.start()
+        thread.timer1 = threading.Timer(2,thread.price_monitoring)
+        thread.timer1.start()
+    else:
+        exit(1)
